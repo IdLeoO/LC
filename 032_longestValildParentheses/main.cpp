af84b3886e6642d8caf96b5be0d9f18530f9bcf0
Weijie_Li <2567860905@qq.com>
2021-12-23 15:18:42 -0500

    Solved P32

--- 032_longestValildParentheses/main.cpp
+++ 032_longestValildParentheses/main.cpp
@@ -1,40 +1,87 @@
 #include "main.hpp"
 #include <iostream>
 #include <algorithm>
-#include <unordered_map>
+#include <vector>
+#include <stack>
 
 using namespace std;
 
 int Solution::longestValidParentheses(string s){
-    if (s == ""){
-        return 0;
+    vector<int> dp;
+    int ans = 0;
+    if (s.length() == 0){
+        return ans;
     }
-    unordered_map<char, int> map;
-    map['('] = 0;
-    int maxcnt = 0;
-    int cnt = 0;
+    stack<int> storage;
     for (int i = 0; i < s.length(); i++){
         if (s[i] == '('){
-            map['('] ++;
-        }
-        else if (s[i] == ')' && map['('] > 0){
-            map['('] --;
-            cnt ++;
+            storage.push(i);
+            dp.push_back(0);
         }
         else{
-            maxcnt = max(cnt, maxcnt);
-            cnt = 0;
+            if (!storage.empty()){
+                int left = storage.top();
+                storage.pop();
+                int len = i - left + 1;
+                if (left != 0){
+                    dp.push_back(dp[left-1] + len);
+                }
+                else{
+                    dp.push_back(dp[left] + len);
+                }
+                ans = max(ans, dp[i]);
+            }
+            else {
+                dp.push_back(0);
+            }
         }
     }
-    if (map['('] == 0){
-        maxcnt = max(cnt, maxcnt);
-    }
-    return 2* maxcnt;
+    return ans;
 }
+
+// Counting the number of ( and ) while traversing the string. Traverse in reverse
+// order later.
+int longestValidParentheses(string s) {
+        int left = 0, right = 0, ans = 0;
+        for (int i = 0; i < s.length(); i++){
+            if (s[i] == '('){
+                left ++;
+            }
+            else{
+                right ++;
+            }
+            if (left == right){
+                ans = max(ans, 2 * left);
+            }
+            else if (right >= left){
+                right = 0;
+                left = 0;
+            }
+        }
+        
+        left = 0, right = 0;
+        for (int i = s.length() - 1; i >= 0 ; i--){
+            if (s[i] == '('){
+                left ++;
+            }
+            else{
+                right ++;
+            }
+            if (left == right){
+                ans = max(ans, 2 * left);
+            }
+            else if (left >= right){
+                right = 0;
+                left = 0;
+            }
+        }
+        return ans;
+    }
+
 
 int main(int argc, char* argv[]){
     Solution sol;
-    string s = "(()";
+    string s = ")()()()((())))";
     cout << sol.longestValidParentheses(s) <<endl;
     return 0;
 }
